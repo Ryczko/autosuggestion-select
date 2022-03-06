cf5dc8a406c2f0acb22a09089d1502c0b46e45a5
Ryczko <konradryczko1@gmail.com>
2022-03-06 15:33:39 +0100

    Add autosuggestion component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AutosuggestionSelect from "./AutosuggestionSelect/AutosuggestionSelect";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const onSelectionChange = (options: string[]) => {
+        console.log(options);
+    };
+
+    return (
+        <div>
+            <AutosuggestionSelect actionOnChange={onSelectionChange} name="Universities"></AutosuggestionSelect>
+        </div>
+    );
 }
 
 export default App;
